137fc96b0401ea97fec9781a97b74dab633a9ecc
Florian Mailliet <florian.mailliet@dailymotion.com>
2018-03-27 16:47:37 +0200

    Simplify requestAnimationFrame polyfill

--- src/util.js
+++ src/util.js
@@ -14,31 +14,11 @@ export function filterNullChildren(children) {
 	return children && children.filter(i => i !== null);
 }
 
-export const requestAnimationFrame = (() => {
-	let raf;
-
-	if (typeof window !== 'undefined') {
-		raf = window.requestAnimationFrame;
-		const prefixes = ['ms', 'moz', 'webkit', 'o'];
-		for (let i = 0; i < prefixes.length; i++) {
-			if (raf) break;
-			raf = window[`${prefixes[i]}RequestAnimationFrame}`];
-		}
+export const requestAnimationFrame = (callback) => {
+	if (typeof window !== 'undefined' && window.requestAnimationFrame) {
+		window.requestAnimationFrame(callback);
 	}
-
-	if (!raf) {
-		let timeLast = 0;
-		raf = (callback) => {
-			const timeCurrent = new Date().getTime();
-
-			/* Dynamically set the delay on a per-tick basis to more closely match 60fps. */
-			/* Technique by Erik Moller. MIT license: https://gist.github.com/paulirish/1579671. */
-			const timeDelta = Math.max(0, 16 - (timeCurrent - timeLast));
-			timeLast = timeCurrent + timeDelta;
-
-			return setTimeout(() => { callback(timeCurrent + timeDelta); }, timeDelta);
-		};
+	else {
+		setTimeout(callback, 17);
 	}
-
-	return raf;
-})();
+};
